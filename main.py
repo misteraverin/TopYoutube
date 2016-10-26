5a5e4fbe9d71937bae589099a02563105a7ed602
Maxim Averin <amax0703@gmail.com>
2016-10-26 14:35:44 +0300

    first worked version

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 __author__ = "Maxim Averin"
+# -- coding: utf-8 --
 
 from bs4 import Tag
 from bs4 import BeautifulSoup
+import urllib.request
+import json
 
-#constants
+# constants
 API_KEY = "AIzaSyAc5hS42qW0TcJ8kGjQct6eIF13teg59VA"
+BASE_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet"
+MAX_RESULT = 10
+
+best_videos = []
+
 
 class Video:
-    def __init__(self, title, data, comments=0, likes=0, watches=0):
+    def __init__(self, title, data, description, comments=0, likes=0, watches=0):
         self.title = title
         self.data = data
+        self.description = description
         self.comments = comments
         self.likes = likes
         self.watches = watches
@@ -21,22 +30,58 @@ class Video:
 
 
 def update_information():
-    html = open('index1.html', 'r', encoding="utf-8")
+    html = open('index.html', 'r', encoding="utf-8")
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser')
-    for a in soup.find_all('a'):
-        a.string.replace_with("Alice!")
+
+    tbody_found = soup.find(class_='videos')
+    for i, video in enumerate(best_videos):
+        tr = soup.new_tag('tr')
+        #id
+        th_id = soup.new_tag('th')
+        th_id.string = str(i + 1)
+        tr.append(th_id)
+        # title
+        th_title = soup.new_tag('th')
+        th_title.string = video.title
+        tr.append(th_title)
+        # data
+        th_data = soup.new_tag('th')
+        th_data.string = video.data
+        tr.append(th_data)
+        # description
+        th_description = soup.new_tag('th')
+        th_description.string = video.description
+        tr.append(th_description)
+        tbody_found.append(tr)
     html.close()
 
     output_file = open('index2.html', 'w')
-    print(soup.prettify(), file=output_file)
+    print(soup.prettify('latin-1').decode('utf-8'), file=output_file)
     output_file.close()
 
 
+def get_videos():
+    time = "2016-10-26T00:00:00Z"
+    GET_QUERY = BASE_URL + "&maxResults=" + str(MAX_RESULT) + "&publishedAfter=" + time + "&key=" + API_KEY
+    request = urllib.request.urlopen(GET_QUERY)
+
+    videos = json.loads(request.read().decode(request.info().get_param('charset') or 'utf-8'))
+    count = 0
+    for video in videos['items']:
+        try:
+            count += 1
+            best_videos.append(Video(video['snippet']['title'], video['id']['videoId'], video['snippet']['publishedAt']))
+            print("New video №", count)
+            print("Video ID: ", video['id']['videoId'])
+            print("Video Title: ", video['snippet']['title'])
+            print("Data: ", video['snippet']['publishedAt'])
+        except:
+            continue
+
 
 def main():
-    # first_video = Video("Cool video, dude!", "22.06")
-    #  first_video.debug_print()
+    get_videos()
     update_information()
 
 
--- readme.md
+++ readme.md
@@ -3,15 +3,16 @@ It is a cool Youtube scrapper which collects top videos from past 7 days. It has
 
 TODO:
 
-* edit css, when item is clicked, it became active
-* edit css for tables and make media queries on phones
-* API_KEy and make response and get json
-* parse json and sort items
-* update html table
-* get_time
+- [ ] edit css, when item is clicked, it became active
+- [ ] edit css for tables and make media queries on phones
+- [x] API_KEY and make response and get json
+- [ ] parse json and sort items
+- [x] update html table
+- [ ] get_time
+- [ ] problems with charset, read article about it and check charset of Beutiful soup and
+request
 
 Read:
 
-* __main__
 * beautifulsoup api
-* http://www.pythonforbeginners.com/api/using-the-youtube-api
+* https://developers.google.com/youtube/v3/docs/search/list#try-it
